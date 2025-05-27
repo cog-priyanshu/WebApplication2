b8ca223cf59a071959ac22303bb08d767bb761dc
cog-priyanshu <Priyanshu.Yadav@cognizant.com>
2025-05-27 12:51:52 +0530

    Init commit

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726d1881553876568ce60ebd65c2e4d9be3cab44")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
